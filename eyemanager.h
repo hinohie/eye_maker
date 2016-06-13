fc3a44fe343fccd05566e838184b5b7637a3e012
hinohie <h.pichulia@gmail.com>
2016-06-13 23:10:25 +0900

    Push src. How to make folder?
    arrrrr

---
+++ eyemanager.h
@@ -0,0 +1,34 @@
+#ifndef __EYE_MANAGER_H__
+#define __EYE_MANAGER_H__
+
+#include <algorithm>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+
+#include <math.h>
+#include <time.h>
+
+#include "vector2f.h"
+
+class EyeManager{
+public:
+	vector<vector2f> dst;
+	vector<vector2f> src;
+	vector<CvScalar> src_color;
+	vector<vector2f> d;
+	vector3f sal_col;
+	vector2f dir;
+
+	IplImage *eye;
+	vector<vector2d> eye_vec;
+
+	EyeManager(){}
+	EyeManager(IplImage *eye_dst, IplImage *eye_src, IplImage *eye, vector<vector2d> &_eye_vec,IplImage *img, vector2f _dir);
+
+	void build();
+	void update_image(IplImage *img,double t);
+	vector<int> *ijk;
+	bool ijk_init;
+};
+
+#endif
